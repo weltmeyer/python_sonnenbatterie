839dd6304a7b7f55c582001a24a5d94831b0027f
Jan Weltmeyer <weltmeyer@outlook.com>
2023-07-21 10:52:53 +0200

    Update make_dist.bat
    add some reminders in make_dist for me

--- make_dist.bat
+++ make_dist.bat
@@ -1,2 +1,7 @@
+REM pip install twine
+REM pip install wheel
+
+REM set path=%path%;%LOCALAPPDATA%\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts
+
 python setup.py sdist bdist_wheel
 twine check dist/*
