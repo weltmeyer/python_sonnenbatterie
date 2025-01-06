a57fca478919fb8b26391ee76dfb28f090c73a88
stefan <stefan@whocares.de>
2025-01-06 15:59:26 +0100

    Disable some selected IDE warnings
    Signed-off-by: stefan <stefan@whocares.de>

--- sonnenbatterie/sonnenbatterie.py
+++ sonnenbatterie/sonnenbatterie.py
@@ -13,6 +13,7 @@ from sonnenbatterie2 import SonnenBatterieV2
 from .const import *
 
 
+# noinspection PyPep8Naming
 class sonnenbatterie:
     # noinspection HttpUrlsUsage
     def __init__(self,username,password,ipaddress):
@@ -417,6 +418,7 @@ class AsyncSonnenBatterie:
             raise Exception(f"Reserve must be between 0 and 100, you spcified {reserve}")
         return await self.set_configuration(SONNEN_CONFIGURATION_BACKUP_RESERVE, reserve)
 
+    # noinspection DuplicatedCode
     async def set_battery_reserve_relative_to_current_charge(self, offset=0, min_res=0) -> json:
         current_level = await self.get_current_charge_level()
         target_level = current_level + offset
